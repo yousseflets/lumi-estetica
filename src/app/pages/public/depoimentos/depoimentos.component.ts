97d7b3c0f8f1de6649f22dc92ef79095a3d58290
leticia.youssef <leticiayoussef@movida.com.br>
2026-08-11 22:34:55 -0300

    ajustes

--- src/app/pages/public/depoimentos/depoimentos.component.ts
+++ src/app/pages/public/depoimentos/depoimentos.component.ts
@@ -8,5 +8,10 @@ import { DataService } from '../../../core/services/data.service';
   styleUrl: './depoimentos.component.scss'
 })
 export class DepoimentosComponent {
+  readonly niveis = [1, 2, 3, 4, 5];
+  readonly titulo = 'Hist\u00f3rias de cuidado';
+  readonly subtitulo = 'A confian\u00e7a das nossas clientes \u00e9 o que mais nos move.';
+  readonly sep = '\u00b7';
+
   constructor(public data: DataService) {}
 }
